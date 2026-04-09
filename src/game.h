3ce0fab797b0e49f9808900c61dbff24576d7b75
bylga <vs.kislyakov@gmail.com>
2026-04-09 14:09:11 +0300

    Starter work on multilevel system, removed bin

--- src/game.h
+++ src/game.h
@@ -1,15 +1,24 @@
 #include<ncurses.h>
 #include<assert.h>
+#include<string.h>
 #define sidex 32
-#define sidey 16
+#define sidey 32
 #define STARTX 16
 #define STARTY 3
 #ifndef GAMEIMPL
 #define GAMEIMPL
 #else
 
+int CurLevel = 0;
 char board[sidex * (sidey + 1) + 1];
 long long Turn = 0;
+
+enum Levels {
+	test = 0,
+	test1,
+	END
+};
+/* TODO: Imploment debug level selection */
 
 typedef struct{
 	int x;
@@ -22,7 +31,9 @@ int clamp(int x, int left, int right){
 	return x;
 }
 
-int movePlayer(int mv, Player * p){
+int movePlayer(int mv, Player * p,  FILE * Level){
+	int mX = 32; int mY = 32;
+	enum Levels lvl = END;
 	switch (mv){
 		case KEY_UP:
 			p->y--;
@@ -37,13 +48,42 @@ int movePlayer(int mv, Player * p){
 			p->x++;
 			break;
 		case 'P':
+			if (Level != NULL) {fclose(Level); Level = NULL;}
 			return 1;
+		case 'C':
+			CurLevel += 1;
+			CurLevel %= END;
+			return 0;
 		default:
-			return 2;
+			return 0;
 	}
-	p->x = clamp(p->x, 1, sidex);
-	p->y = clamp(p->y, 1, sidey);
+	p->x = clamp(p->x, 1, mX);
+	p->y = clamp(p->y, 1, mY);
 	return 0;
 }
 
+void itoa(char buf[], int x, size_t size){
+	size--;
+	buf[size] = '\0';
+	while (size > 0){
+		size--;
+		buf[size] = x % 10 + '0';
+		x /= 10;
+	}
+}
+
+FILE * LoadLevel(int LevelNumber){
+
+	char __BUF[6];
+	char lvl[BUFSIZ] = "";
+	FILE * Level;
+	itoa(__BUF, LevelNumber, 6);
+
+	strcat(lvl, "./levels/lvl"); strcat(lvl, __BUF); strcat(lvl, ".lvl");
+
+	Level = fopen(lvl, "r");
+	if (Level == NULL) fprintf(stderr, "There is no such level as %d", CurLevel);
+
+	return Level;
+}
 #endif
--- src/main.c
+++ src/main.c
@@ -1,31 +1,56 @@
+/*
+TODO: Add inventory      |- priority 1
+TODO: Add other creatures|
+TODO: Add saves          |- priority 2
+*/
 #define GAMEIMPL
 #include"game.h"
+
+void printLevel(FILE * Level){
+	fprintf(stdout,"printing level...\n");
+	
+}
+
+void GenMap(){
+	board[sidex * sidey] = '\0';
+	for (int i = 1; i < sidey * (sidex + 1); i++){
+		if (i % (sidex + 1) == 0) board[i - 1] = '\n';
+		else board[i - 1] = '.';
+	}
+}
 
 int main(){
 	Player p = {0};
 	p.x = STARTX;
 	p.y = STARTY;
-	
+	FILE * Level = NULL;
+	int mX = 0, mY = 0;
 	initscr();
 	noecho();
 	cbreak();
 	keypad(stdscr, TRUE);
-	int c;
 
 	do{
-		board[sidex * sidey] = '\0';
-		for (int i = 1; i < sidey * (sidex + 1); i++){
-			if (i % (sidex + 1) == 0) board[i - 1] = '\n';
-			else board[i - 1] = '.';
+		if (CurLevel == 0){
+			if (Level != NULL) {fclose(Level); Level = NULL;}
+			GenMap();
+			board[p.x - 1 + (sidex + 1) * (p.y - 1)] = '@';
+			mX = 32; mY = 16;
+			printw("%s\n", board);
 		}
-
-		board[p.x - 1 + (sidex + 1) * (p.y - 1)] = '@';
-
-		printw("%s\n", board);
+		else{
+			if (Level == NULL){
+				Level = LoadLevel(CurLevel);
+				printLevel(Level);
+			}
+			else{
+				printLevel(Level);
+			}
+		}
 		printw("\n x: %4d, y: %4d", p.x, p.y);
 		move(0,0);
 		refresh();
-	} while((movePlayer(getch(), &p) == 0));
+	} while((movePlayer(getch(), &p,  Level) == 0));
 
 	endwin();
 	return 0;
