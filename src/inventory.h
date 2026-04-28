17e217b7277115c06ab41a9847d94e68234a0c87
bylga <vs.kislyakov@gmail.com>
2026-04-28 12:56:45 +0300

    decided not to use binary trees, because they aren't needed

--- src/inventory.h
+++ src/inventory.h
@@ -10,59 +10,17 @@ typedef struct{
 	char type;
 } Item;
 
-#define INIT_NODE(n, a) n->left=NULL;n->right=NULL;n->item=a
-
-typedef struct Node Node;
-struct Node{
-	Node * left;
-	Node * right;
-	Item item;
-};
-
 typedef struct{
-	Node * Root;
+	Item * items;
+	size_t capacity;
 	size_t count;
 	int x;
 	int y;
 	int level;
 } Inventory;
 
-void RemoveNode (Node * Root, Node * prev, Item * a);
-void AddNode(Node *, Item *, Node *);
+#define AddItem(i, a) 
 
-#define AddItem(i, a) AddNode(i->Root, &a, NULL)
-#define RemoveItem(i, a) RemoveNode(i->Root, NULL, a)
-
-void AddNode(Node * Root, Item * item, Node * itemNode){
-	if (itemNode == NULL) {Node * newNode = malloc(sizeof(Node)); itemNode = newNode;}
-	INIT_NODE(itemNode, *item);
-	if (Root->right != NULL){
-		int v = strcmp(Root->item.name, item->name);
-		switch (v > 0){
-			case (0): 
-			switch (v == 0){
-				case (1):
-				if (itemNode != NULL) free(itemNode);
-				Root->right->item.amount += item->amount;
-				break;
-				case (0):
-				AddNode(Root->left, item, itemNode);
-				break;
-			}
-			break;
-
-			case (1):
-			if (Root->left != NULL){
-				AddNode(Root->left, item, itemNode);
-			}
-			else{
-				Root->left = itemNode;
-			}
-			break;
-		}
-	} else{
-			Root->left = itemNode;
-	}
-}
+#define RemoveItem(i, a)
 
 #endif
